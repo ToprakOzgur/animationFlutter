240aee2d42d490e886ebb8f1863cc5a29ef86996
ToprakOzgur <zgrtprk@gmail.com>
2019-03-12 11:38:46 +0300

    first commit

---
+++ lib/src/widgets/cat.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Cat extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Image.network(
+      'https://image.spreadshirtmedia.com/image-server/v1/mp/compositions/T812A1MPA3140PT17X208Y0D1015735634FS5600/views/1,width=800,height=800,appearanceId=1,backgroundColor=FFFFFF,noPt=true,version=1543820067/juni-cat-icon-mens-premium-t-shirt.jpg',
+    );
+  }
+}
